6994ef64e82c40434f31071e0ab8ec3b9b87ca6a
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-08-07 22:41:33 +0300

    Add registration form and update home view to handle form submission

--- dummy_project/app/views.py
+++ dummy_project/app/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
 from django.http import HttpResponse
+from app.forms import ExampleForm
 
 
 def register(request):
     return render(request, 'register.html')
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        print(form.data) 
+        print(form.errors)
+        return redirect('home')
+    else:
+        form = ExampleForm()
+        return render(request, 'index.html', {"form":form})
