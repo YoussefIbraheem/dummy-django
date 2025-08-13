1ee0d65d9997482414b6b83d21b592ea70e2f629
youssef <youssefm.muhammed@gmail.com>
2025-08-13 22:07:23 +0300

    Add article listing and detail views; update templates and URLs for articles

--- task/app/views.py
+++ task/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from app.models import Article
+from django.utils.translation import gettext as _
 
-# Create your views here.
+def article_list(request):
+    articles = Article.objects.all()
+    
+    return render(request, 'app/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    
+    return render(request, 'app/article_detail.html', {'article': article})
+
+
+
--- task/mysite/settings.py
+++ task/mysite/settings.py
@@ -104,7 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
+
+# 'en-us'
 
 TIME_ZONE = 'UTC'
 
