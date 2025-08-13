1279f5ddeb402995241d7ebe567657c3a81878a5
youssef <youssefm.muhammed@gmail.com>
2025-08-13 22:19:41 +0300

    Change default LANGUAGE_CODE from 'ar' to 'en-us' in settings

--- task/mysite/settings.py
+++ task/mysite/settings.py
@@ -104,9 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ar'
-
-# 'en-us'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
